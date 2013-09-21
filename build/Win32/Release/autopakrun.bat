9527f54f82f3fbe62df885775c9a8d39ce4aaa9f
Robin Thunstrom <robin.thunstrom@gmail.com>
2013-09-21 12:39:24 +0200

    Trimmed the bat-file

--- build/Win32/Release/autopakrun.bat
+++ build/Win32/Release/autopakrun.bat
@@ -1,21 +1,20 @@
 @echo off
 if exist "gamex86.dll" (
-"autopak.bat"
-:bootup
-timeout /T 1
-echo JARL IS BATMAN
+	"autopak.bat"
+	:bootup
+	timeout /T 1
+	echo JARL IS BATMAN
 
-cd ..\Dedicated Release
-START "DOOMsrv" "DOOM3.exe" +exec "default.cfg"
+	cd ..\Dedicated Release
+	START "DOOMsrv" "DOOM3.exe" +exec "default.cfg"
 
-cd ..\Release
-START "DOOM" "DOOM3.exe" +disconnect +developer "1" +connect "127.0.0.1"
+	cd ..\Release
+	START "DOOM" "DOOM3.exe" +disconnect +developer "1" +connect "127.0.0.1"
 
-timeout /T 2
-EXIT
-) else (
-goto bootup
-EXIT
+	timeout /T 2
+	EXIT
+) 
+else (
+	goto bootup
 )
-EXIT
 
